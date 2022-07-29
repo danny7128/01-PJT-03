273c7167c8609f848edbda4a69e0561756f53ae4
danny7128 <danny7128@gmail.com>
2022-07-29 17:27:21 +0900

    1기_3회차_이성훈_2022_07_29_제출

--- 3회차/이성훈/1_소득불균형.py
+++ 3회차/이성훈/1_소득불균형.py
@@ -1,3 +1,22 @@
 import sys
 
 sys.stdin = open("_소득불균형.txt")
+
+T = int(input())
+cnt = 0
+cnt_sum = []
+
+
+for i in range(1,T+1):
+    N = int(input())
+    A = list(map(int,input().split()))
+    avg_ = sum(A)/len(A)
+    for j in A:
+        if j <= avg_:
+            cnt += 1
+    print(f'#{i} {cnt}')
+    cnt = 0
+
+
+
+
--- 3회차/이성훈/2_문자열의거울상.py
+++ 3회차/이성훈/2_문자열의거울상.py
@@ -1,3 +1,22 @@
 import sys
 
 sys.stdin = open("_문자열의거울상.txt")
+
+T = int(input())
+
+
+for j in range(1,T+1):
+    A = input()
+    A_rev = A[::-1]
+    A_rev_ = []
+    print(f'#{j}',end = ' ')
+    for i in A_rev:
+        if i == 'q':
+            print('p',end='')
+        elif i == 'p':
+            print('q',end='')
+        elif i == 'b':
+            print('d',end='')
+        else:
+            print('b',end='')
+    print()
